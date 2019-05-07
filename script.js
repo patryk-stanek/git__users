fb5f55c9805f1d0a77eaec563b585e2cedc63e59
Patryk Stanek <p.stanek3@gmail.com>
2019-05-07 17:10:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            searchText: '',
+            users: []
+        };
+    }
+
+    onChangeHandle(event) {
+        this.setState({searchText: event.target.value});
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        const {searchText} = this.state;
+        const url = `http://api.github.com/search/users?q=${searchText}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(responseJson => this.setState({users: responseJson.items}));
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={event => this.onSubmit(event)}>
+                    <label htmlFor="searchText">Search by user name</label>
+                    <input
+                        type="text"
+                        id="searchText"
+                        onChange={event => this.onChangeHandle(event)}
+                        value={this.state.searchText}/>
+                </form>
+                <UsersList users={this.state.users}/>
+            </div>
+        );
+    }
+}
+
+class UserList extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/>
+                <a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
+            </div>
+        )
+    }
+}
